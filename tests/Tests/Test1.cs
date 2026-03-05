bb084740184eecaf3421f8e130eb1883350c7834
Louise Scheel Hansen <lsha59799@edu.ucl.dk>
2026-03-05 12:29:56 +0100

    Removal of text

--- tests/Tests/Test1.cs
+++ tests/Tests/Test1.cs
@@ -6,6 +6,6 @@ public sealed class Test1
     [TestMethod]
     public void TestMethod1()
     {
-        // This is a placeholder test method. You can replace it with actual test logic.
+        
     }
 }
