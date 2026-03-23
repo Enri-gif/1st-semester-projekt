dec4894f2c25fbf2cf2d62f4588c72def2735014
Greyrib <mlabildgaard@live.dk>
2026-03-23 13:43:56 +0100

    Weather stuff removed from Program.cs, Weather.razor & the navmenu item
    Also searched through solution for
    @inject Weather
    Weather
    WeatherForecastService
    FetchData
    to find any remnants, without any hits.

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -84,25 +84,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.UseCors ("AllowWasmClient");
 
 app.UseAuthentication ();
@@ -111,8 +92,3 @@ app.UseAuthorization ();
 app.MapControllers ();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
